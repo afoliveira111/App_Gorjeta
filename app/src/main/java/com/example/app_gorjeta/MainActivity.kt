ca83386360726600449e6cb052ea0db3099355aa
Felipe <felipetermometria@hotmail.com>
2024-04-15 11:27:29 -0300

    Enable viewbinding

--- app/src/main/java/com/example/app_gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/app_gorjeta/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.app_gorjeta
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app_gorjeta.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
 
 
-
-
+        binding.btnClean.setOnClickListener{
+            println("Felipe1 " + binding.tieTotal.text)
+            println("Felipe1 " + binding.tieNumPeople.text)
+        }
 
 
 
