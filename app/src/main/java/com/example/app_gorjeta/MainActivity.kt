7c2b9deb1c025e37894dcbf0f8ff27994cf67e96
Felipe <felipetermometria@hotmail.com>
2024-04-18 14:37:55 -0300

    Create Spinner

--- app/src/main/java/com/example/app_gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/app_gorjeta/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.app_gorjeta
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -41,7 +44,33 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerNumberOfPeople.adapter = adapter
+
+
+        var numOfPeopleSelected = 0
+        binding.spinnerNumberOfPeople.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    numOfPeopleSelected = position
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+            }
 
         binding.btnDone.setOnClickListener{
             val totalTableTemp = binding.tieTotal.text
@@ -53,7 +82,7 @@ class MainActivity : AppCompatActivity() {
                         .show()
             } else {
                 val totalTable: Float = totalTableTemp.toString().toFloat()
-                val nPeople: Int = 5
+                val nPeople: Int = numOfPeopleSelected
 
                 val totalTemp = totalTable / nPeople
                 val tips = totalTemp * percentage / 100
@@ -68,7 +97,6 @@ class MainActivity : AppCompatActivity() {
                     binding.rbOptionOne.isChecked = false
                     binding.rbOptionTwo.isChecked = false
                 }
-
         }
     }
 }
